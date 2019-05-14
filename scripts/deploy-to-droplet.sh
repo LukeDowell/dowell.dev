c6b5829f639d164acd370595005b5c6660bbdd20
lukedowell@gmail.com <=>
2019-05-14 10:39:13 -0500

    Skips authenticity check for ssh step

--- scripts/deploy-to-droplet.sh
+++ scripts/deploy-to-droplet.sh
@@ -6,7 +6,7 @@ chmod 600 .travis/travis_rsa # Allow read access to the private key
 ssh-add .travis/travis_rsa # Add the private key to SSH
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh $SSH_USERNAME@$IP <<EOF
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $SSH_USERNAME@$IP <<EOF
     docker stop dowell.dev
     docker rm dowell.dev
     docker run -d -p 80:8080 --name dowell.dev lukedowell/dowell.dev:${TAG}
