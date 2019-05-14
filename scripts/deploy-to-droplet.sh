9c0ed6d448e64d81964c8429170c9b71424beffd
lukedowell@gmail.com <=>
2019-05-14 16:13:53 -0500

    The true CI test

--- scripts/deploy-to-droplet.sh
+++ scripts/deploy-to-droplet.sh
@@ -5,7 +5,7 @@ chmod 600 .travis/travis_rsa # Allow read access to the private key
 ssh-add .travis/travis_rsa # Add the private key to SSH
 
 ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $SSH_USERNAME@$IP <<EOF
-    docker stop dowell.dev
-    docker rm dowell.dev
-    docker run -d -p 80:8080 --name dowell.dev lukedowell/dowell.dev:${RELEASE_TAG}
+    docker stop dowell-dev
+    docker rm dowell-dev
+    docker run -d -p 8080 --name dowell-dev --network travis_nginx-network lukedowell/dowell.dev:${RELEASE_TAG}
 EOF
