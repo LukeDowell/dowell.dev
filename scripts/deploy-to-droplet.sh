e33226da534d7f2de0b5fdea8edcad3a59b6f88f
lukedowell@gmail.com <=>
2019-05-14 10:18:03 -0500

    Second attempt at encrypted travis yml and extracting deploy scripts

--- scripts/deploy-to-droplet.sh
+++ scripts/deploy-to-droplet.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+#!/bin/bash
+
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/travis_rsa # Allow read access to the private key
+ssh-add .travis/travis_rsa # Add the private key to SSH
+
+# Skip this command if you don't need to execute any additional commands after deploying.
+ssh $SSH_USERNAME@$IP <<EOF
+
+EOF
--- scripts/push-docker-image.sh
+++ scripts/push-docker-image.sh
@@ -4,5 +4,6 @@ echo "Building and pushing dowell.dev to docker hub..."
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker build -t dowell.dev .
 docker tag dowell.dev lukedowell/dowell.dev:$RELEASE_TAG
+docker tag dowell.dev lukedowell/dowell.dev:latest
 docker push lukedowell/dowell.dev:$RELEASE_TAG
 echo "Finished pushing dowell.dev to docker hub ^_^"
