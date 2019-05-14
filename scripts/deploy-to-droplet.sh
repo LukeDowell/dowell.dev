56e47e23fb357b81991e46b5421a186f9c5062d9
lukedowell@gmail.com <=>
2019-05-14 11:25:46 -0500

    Cleans up gradle and script files

--- scripts/deploy-to-droplet.sh
+++ scripts/deploy-to-droplet.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-TAG=$RELEASE_TAG # I'm scared SSHing in will use the remote's env variables
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/travis_rsa # Allow read access to the private key
 ssh-add .travis/travis_rsa # Add the private key to SSH
 
-# Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $SSH_USERNAME@$IP <<EOF
     docker stop dowell.dev
     docker rm dowell.dev
-    docker run -d -p 80:8080 --name dowell.dev lukedowell/dowell.dev:${TAG}
+    docker run -d -p 80:8080 --name dowell.dev lukedowell/dowell.dev:${RELEASE_TAG}
 EOF
