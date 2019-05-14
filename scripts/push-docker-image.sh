b6f30b8f1ec4dd49891f1b815601542e8f1c93eb
lukedowell@gmail.com <=>
2019-05-14 10:03:00 -0500

    First try with encrypted ssh key

---
+++ scripts/push-docker-image.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Building and pushing dowell.dev to docker hub..."
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+docker build -t dowell.dev .
+docker tag dowell.dev lukedowell/dowell.dev:$RELEASE_TAG
+docker push lukedowell/dowell.dev:$RELEASE_TAG
+echo "Finished pushing dowell.dev to docker hub ^_^"
